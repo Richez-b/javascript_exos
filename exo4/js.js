48900d1d73b1c4f6cb1f95d3dc3c2b1b41f935e5
Bradley <richez.bradley@gmail.com>
2025-01-08 11:49:58 +0100

    add exo4

--- exo4/js.js
+++ exo4/js.js
@@ -2,14 +2,6 @@
 let input = document.querySelector('textarea');
 let div = document.querySelector('div');
 
-input.addEventListener('keydown',(e)=>{
-    console.log(e.key)
-    if(e.key=='Enter'){
-        saut = document.createElement('br');
-        div.append(saut)
-    }
-})
-
 input.addEventListener('input',(e)=>{
     localStorage.setItem("monSuperTexte",div.innerHTML = e.target.value); 
 });
